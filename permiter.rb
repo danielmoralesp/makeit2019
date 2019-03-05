c0aec172223cde10870fcc56e4b01775fec5836e
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-05 17:59:38 -0500

    ruby basics 2

---
+++ permiter.rb
@@ -0,0 +1,9 @@
+#lado = 5
+
+print "Ingresar el lado del cuadrado: "
+lado = gets.chomp.to_i
+
+
+puts "el perimetro de un cuadrado de lado #{lado} es #{lado * 4}"
+
+puts "el area de un cuadrado de lado #{lado} es #{lado * lado}"
---
+++ repite.rb
@@ -0,0 +1,3 @@
+repite = gets.chomp
+
+actual_year = Time.now.strftime('%Y').to_i
