6bcaaf5d605bf201d82b95ce602cc17dcff35b81
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-04 20:05:27 -0500

    ruby basics

---
+++ hola_mundo.rb
@@ -0,0 +1,39 @@
+# comentario
+
+var1 = "Variable Numero 1"
+num1 = 4
+num2 = 10
+
+puts "Hola Mundo #{num1 + num2} y esta variable #{var1}"
+
+puts 'Segunda Linea #{4 + 5}'
+
+puts "Hola desde \"mi\" cpu"
+
+puts 1 + 2
+puts 5 * 5
+puts "m" * 5
+
+puts 1 + 2
+
+puts true
+puts false
+
+
+ puts 5 > 3
+ puts 4 == 4
+ puts 4 != 3
+ puts 4 < 10
+ puts 3 >= 10
+
+ puts "Hola " + "Mundo " + "Desde aqui"
+
+ puts "Hola" + 3.to_s
+
+ puts "Hola 2 + 3 = #{2 + 3}"
+
+ puts "Hola 2 + 3 = " + 2.to_s + 3.to_s
+
+ puts "Hola 2 + 3 = " + (2 + 3).to_s
+
+puts 4 % 2
