95ce8e155926b8ac7d16c3080bced6e630062622
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-08 21:02:18 -0500

    POO

--- poo.rb
+++ poo.rb
@@ -1,32 +1,15 @@
 # Clase
 class Person
+  attr_accessor :name
+  # attr_reader :name
+  # attr_writter :name
+
   def initialize(name)
-    # Se ejecuta siempre al momento de instanciar o crear un nuevo objeto
-    # puts "creando una nueva persona con nombre: #{name}..."
-    # Variable de instancia o atributo de clase
     @name = name
   end
 
   def saludar2(otro_nombre)
     "Hola #{otro_nombre}, me llamo #{@name}"
-  end
-
-                                  #############
-                                  # Como se hacia antes
-
-
-                                  #############
-
-  # Metodo para @name pueda ser leido  desde afuera de la clase
-  # getter = Leer
-  def name
-    @name
-  end
-
-  # Metodo para que @name pueda ser modificado desde afuera
-  # setter = Modificar
-  def name=(name)
-    @name = name
   end
 end
 
