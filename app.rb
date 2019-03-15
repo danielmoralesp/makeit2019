20db84c22e1c3ce1072566130ce6dbd321fc908f
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-14 21:34:40 -0500

    Sinatra I

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+get '/' do
+  "Hola Mundo!"
+end
+
+get '/search' do
+  "Esta es la busqueda"
+end
+
+root /
+path /
+home /
+raiz /
+
+www.mysite.com/
+www.mysite.com/search
+
+hhtp://localhost:3000/search
+http://127.0.0.0:3000/search
