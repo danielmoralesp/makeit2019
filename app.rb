8c23f43869476efed9964a493dd8e3894b3a064c
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-15 20:13:41 -0500

    Sinatra II

--- app.rb
+++ app.rb
@@ -1,20 +1,73 @@
 require 'sinatra'
 
+# get '/' do
+#   "Hola Mundo! modificado"
+# end
+#
+# post '/' do
+#   # crear un recurso
+# end
+#
+# put '/' do
+#   # actualizar un recurso
+# end
+#
+# patch '/' do
+#   #actualizar recurso
+# end
+#
+# delete '/' do
+#   # eliminar recurso
+# end
+#
+#
+#
+# get '/user' do
+#   "Leyendo un recurso"
+# end
+#
+# post '/user' do
+#   "Enviando un formulario"
+# end
+
+
+
+# get 'post/:id' do
+#   "Hola #{params[:nombre]} #{params[:apellido]}"
+# end
+
 get '/' do
-  "Hola Mundo!"
+
+  if @codigo == "2019-03-15 19:05:16 -0500"
+    @codigo = Time.now
+  else
+    @codigo2 = "String"
+  end
+
+  erb :index
 end
 
-get '/search' do
-  "Esta es la busqueda"
+get '/ruta1' do
+  erb :ruta1
 end
 
-root /
-path /
-home /
-raiz /
 
-www.mysite.com/
-www.mysite.com/search
 
-hhtp://localhost:3000/search
-http://127.0.0.0:3000/search
+
+# get '/search' do
+#   "Esta es la busqueda"
+# end
+
+# root /
+# path /
+# home /
+# raiz /
+#
+# www.mysite.com/
+# www.mysite.com/search
+#
+# hhtp://localhost:3000/search
+# http://127.0.0.0:3000/search
+
+# hhtp://localhost:4567
+# http://127.0.0.0:4567
