6614f656a674f2c4694122b9e5581943ecc26cdc
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-21 17:51:19 -0500

    Delete resuelto

--- proyecto_sinatra/index.rb
+++ proyecto_sinatra/index.rb
@@ -8,9 +8,10 @@ class Notes
   include DataMapper::Resource
 
   property :id,           Serial # un integer autoincremental
-  property :content,      Text, :required => true, :default => 0
+  property :content,      Text, :required => true
   property :created_at,   DateTime
   property :updated_at,   DateTime
+  property :complete,     Boolean, :required => true, :default => 0
 end
 
 DataMapper.auto_upgrade!
@@ -23,6 +24,52 @@ class MiApp < Sinatra::Base
     @title = "Todas las notas"
     erb :index
   end
+
+  post '/' do
+    n = Notes.new
+    n.content = params[:content]
+    n.created_at = Time.now
+    n.updated_at = Time.now
+    n.save
+    redirect '/'
+  end
+
+  get '/:id' do
+    @note = Notes.get params[:id]
+    @title = "Editar la nota #{@note}"
+    erb :edit
+  end
+
+  post '/:id' do
+    n = Notes.get params[:id]
+    n.content = params[:content]
+    n.complete = params[:complete] ? 1 : 0
+    n.updated_at = Time.now
+    n.save
+    redirect '/'
+  end
+
+  get '/:id/complete' do
+    n = Notes.get params[:id]
+    n.complete = n.complete ? 0 : 1
+    n.updated_at = Time.now
+    n.save
+    redirect '/'
+  end
+
+  get '/note/:id/delete' do
+    @note = Notes.get params[:id]
+    @title = "Confirma el borrado de la nota #{@note}?"
+    erb :delete
+  end
+
+  post '/note/:id/delete' do
+    # Notes.get(params[:id]).destroy
+    n = Notes.get params[:id]
+    n.destroy
+    redirect '/'
+  end
+
 end
 
 ### rutas: get, post, put, delete
