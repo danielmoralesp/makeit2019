197749139d444a0f0d200d718b7131e516a58c0e
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-26 17:58:48 -0500

    Rails first steps

---
+++ my_app/app/controllers/blog_controller.rb
@@ -0,0 +1,26 @@
+class BlogController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  	####
+  	# redirect_to :new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  	#
+  end
+
+  def delete
+  	#
+  end
+end
--- my_app/config/routes.rb
+++ my_app/config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
+  
   root 'welcome#index'
 
-  get 'welcome/index', to: 'welcome#index', as: 'welcome'
+  get 'welcome/index', to: 'index/user/welcome#index', as: 'welcome'
+
+  get 'welcome/about', to: 'welcome#about'
+  get 'welcome/contact_us', to: 'welcome#contact_us'
+  get 'welcome/prices', to: 'welcome#prices'
+
+  get 'blog/index'
+  get 'blog/new'
+  get 'blog/show'
+  get 'blog/edit'
 
 end
